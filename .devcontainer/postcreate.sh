f93cad3aceeb164a4e4541ce0c8304c7b0685537
Mike Foster <i.am.mike.foster@gmail.com>
2024-02-02 05:41:36 +0000

    autoforward port 8000; verbose postcreate

--- .devcontainer/postcreate.sh
+++ .devcontainer/postcreate.sh
@@ -1,4 +1,7 @@
+echo "Removing old venv"
 rm -rf .devcontainer/.venv
+echo "Creating new venv"
 py -m venv .devcontainer/.venv
 . .devcontainer/.venv/bin/activate
+echo "Make gpiozero develop"
 make develop

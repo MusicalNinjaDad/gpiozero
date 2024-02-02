425d59cd74ddae23e58c191c1a420f9328ca14f6
Mike Foster <i.am.mike.foster@gmail.com>
2024-02-02 18:24:19 +0000

    run doc previewer in background when container started

--- .devcontainer/postcreate.sh
+++ .devcontainer/postcreate.sh
@@ -5,3 +5,7 @@ py -m venv .devcontainer/.venv
 . .devcontainer/.venv/bin/activate
 echo "Make gpiozero develop"
 make develop
+echo "Make docs clean"
+make -C docs clean
+echo "Make html docs"
+make -C docs html
